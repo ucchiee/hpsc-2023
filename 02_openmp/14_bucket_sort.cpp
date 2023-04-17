b3acea1e70891b81e0279b8adcd641fe3de72cc5
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-04-17 17:00:28 +0900

    02_openmp/14_bucket_sort.cpp: finished to some extent

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -2,6 +2,25 @@
 #include <cstdlib>
 #include <vector>
 
+std::vector<int> scan(std::vector<int> bucket) {
+  int range = bucket.size();
+  std::vector<int> offset(range,0);
+
+  std::vector<int> tmp(range, 0);
+#pragma omp parallel
+  for (int j=1; j<range; j<<=1) {
+#pragma omp for
+    for (int i=0; i<range; i++) {
+      tmp[i] = offset[i];
+    }
+#pragma omp for
+    for (int i=j; i<range; i++) {
+      offset[i] += tmp[i-j] + bucket[i-j];
+    }
+  }
+  return offset;
+}
+
 int main() {
   int n = 50;
   int range = 5;
@@ -13,14 +32,17 @@ int main() {
   printf("\n");
 
   std::vector<int> bucket(range,0); 
+#pragma omp parallel for
   for (int i=0; i<n; i++)
+#pragma omp atomic update
     bucket[key[i]]++;
-  std::vector<int> offset(range,0);
-  for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+
+  auto offset = scan(bucket);
+
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
-    for (; bucket[i]>0; bucket[i]--) {
+    for (int j = offset[i]; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
