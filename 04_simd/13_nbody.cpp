2bb514c28692ee2f4ec91add85a04e26c5318662
Yusuke Uchida <fantomyuu0623@gmail.com>
2023-05-01 03:18:06 +0900

    04_simd/13_nbody.cpp: N can be mul of 8

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -36,46 +36,49 @@ int main() {
   __m256 zeros = _mm256_setzero_ps();
   __m256 mins = _mm256_set1_ps(-FLT_MAX);
   for(int i=0; i<N; i++) {
-    // ひとまず N は 8 だと仮定する
-    // rx, ry の計算
-    __m256 rx_vec = _mm256_sub_ps(
-      _mm256_set1_ps(x[i]),
-      _mm256_load_ps(x)  // todo
-    );
-    __m256 ry_vec = _mm256_sub_ps(
-      _mm256_set1_ps(y[i]),
-      _mm256_load_ps(y)  // todo
-    );
-    // r の逆数の計算
-    __m256 rr_vec = _mm256_rsqrt_ps(
-      _mm256_add_ps(
-        _mm256_mul_ps(rx_vec, rx_vec),
-        _mm256_mul_ps(ry_vec, ry_vec)
-      )
-    );
-    // mの読み込み
-    __m256 m_vec = _mm256_load_ps(m);  // todo
-    // fx, fy それぞれの減る分を計算
-    __m256 delta_fx_vec = _mm256_mul_ps(
-      _mm256_mul_ps(rx_vec, m_vec),
-      _mm256_mul_ps(rr_vec, _mm256_mul_ps(rr_vec, rr_vec))
-    );
-    __m256 delta_fy_vec = _mm256_mul_ps(
-      _mm256_mul_ps(ry_vec, m_vec),
-      _mm256_mul_ps(rr_vec, _mm256_mul_ps(rr_vec, rr_vec))
-    );
-    // maskの作成
-    __m256 v_cmp = _mm256_max_ps(delta_fx_vec, mins);  // -nan の削除, -nanの箇所を除外する
-    __m256 mask = _mm256_cmp_ps(v_cmp, mins, _CMP_EQ_UQ);
-    // maskのbitが立っているところをzeroにする(fx,fy)
-    delta_fx_vec = _mm256_blendv_ps(delta_fx_vec, zeros, mask);
-    delta_fy_vec = _mm256_blendv_ps(delta_fy_vec, zeros, mask);
-    // 各レーンの和を計算
-    float sum_fx = sum_vec(delta_fx_vec); 
-    float sum_fy = sum_vec(delta_fy_vec); 
-    // fx[i], fy[i]に代入
-    fx[i] -= sum_fx;
-    fy[i] -= sum_fy;
-    printf("%d %g %g\n",i,fx[i],fy[i]);
+    // N は 8 の倍数であると仮定する
+    for (int j=0; j<N; j+=8) {
+
+      // rx, ry の計算
+      __m256 rx_vec = _mm256_sub_ps(
+        _mm256_set1_ps(x[i]),
+        _mm256_load_ps(x+j)  // todo
+      );
+      __m256 ry_vec = _mm256_sub_ps(
+        _mm256_set1_ps(y[i]),
+        _mm256_load_ps(y+j)  // todo
+      );
+      // r の逆数の計算
+      __m256 rr_vec = _mm256_rsqrt_ps(
+        _mm256_add_ps(
+          _mm256_mul_ps(rx_vec, rx_vec),
+          _mm256_mul_ps(ry_vec, ry_vec)
+        )
+      );
+      // mの読み込み
+      __m256 m_vec = _mm256_load_ps(m+j);  // todo
+      // fx, fy それぞれの減る分を計算
+      __m256 delta_fx_vec = _mm256_mul_ps(
+        _mm256_mul_ps(rx_vec, m_vec),
+        _mm256_mul_ps(rr_vec, _mm256_mul_ps(rr_vec, rr_vec))
+      );
+      __m256 delta_fy_vec = _mm256_mul_ps(
+        _mm256_mul_ps(ry_vec, m_vec),
+        _mm256_mul_ps(rr_vec, _mm256_mul_ps(rr_vec, rr_vec))
+      );
+      // maskの作成
+      __m256 v_cmp = _mm256_max_ps(delta_fx_vec, mins);  // -nan の削除, -nanの箇所を除外する
+      __m256 mask = _mm256_cmp_ps(v_cmp, mins, _CMP_EQ_UQ);
+      // maskのbitが立っているところをzeroにする(fx,fy)
+      delta_fx_vec = _mm256_blendv_ps(delta_fx_vec, zeros, mask);
+      delta_fy_vec = _mm256_blendv_ps(delta_fy_vec, zeros, mask);
+      // 各レーンの和を計算
+      float sum_fx = sum_vec(delta_fx_vec); 
+      float sum_fy = sum_vec(delta_fy_vec); 
+      // fx[i], fy[i]に代入
+      fx[i] -= sum_fx;
+      fy[i] -= sum_fy;
+    }
+  printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
